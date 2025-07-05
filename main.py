872c6bc35710a3d96f0bbef7729d7d898b8ca20e
acasarotto <alexcasarotto730@gmail.com>
2025-07-05 17:26:09 -0400

    Movement is configured via WASD

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from constants import *
+from player import *
+
 def main():
     print("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while 1 == 1:
         screen.fill("black", rect=None, special_flags=0)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
